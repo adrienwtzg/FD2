b1a12da48cd313bf2318cf4175a4a76227066d52
John Doe <johndoe@email.com>
2025-04-27 18:05:37 +0200

    Suite

---
+++ model/getBudgetFromCategoryAndYear.php
@@ -0,0 +1,23 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+include '../db/db_connect.php';
+
+$idCategory = (isset($_GET["idCategory"]) ? $_GET["idCategory"] : "");
+$year = (isset($_GET["year"]) ? $_GET["year"] : "");
+
+$db = connectDB();
+$query = $db->prepare("SELECT * FROM budget WHERE 'year' = ? AND 'idCategory' = ?");
+$query->bindParam(1, $idCategory);
+$query->bindParam(2, $year);
+
+if ($query->execute()) {
+  $tab = $query->fetchAll();
+  echo json_encode($tab);
+}
+else {
+  echo json_encode(1);
+}
+?>
--- views/budget.php
+++ views/budget.php
@@ -28,184 +28,166 @@ if (isset($_SESSION['loginError']))
     </div>
     
     <div class="container">
-      <h3 class="text-center">Income</h5>
-      <table id="tableIncome" class="table table-hover table-responsive">
-          <tr>
-            <th style="width: 400px;"></th>
-            <th style="text-align: center; width: 100px;">Jan</th>
-            <th style="text-align: center; width: 100px;">Mar</th>
-            <th style="text-align: center; width: 100px;">Feb</th>
-            <th style="text-align: center; width: 100px;">Apr</th>
-            <th style="text-align: center; width: 100px;">May</th>
-            <th style="text-align: center; width: 100px;">Jun</th>
-            <th style="text-align: center; width: 100px;">Jul</th>
-            <th style="text-align: center; width: 100px;">Aug</th>
-            <th style="text-align: center; width: 100px;">Sep</th>
-            <th style="text-align: center; width: 100px;">Oct</th>
-            <th style="text-align: center; width: 100px;">Nov</th>
-            <th style="text-align: center; width: 100px;">Dec</th>
-          </tr>
+      <h4 class="text-center">Income</h4>
+      <table style="margin-bottom: 5%;" id="tableIncome" class="table table-hover table-responsive">
+          
       </table>
       <br>
-      <table id="tableExpenses" class="table table-hover table-responsive">
-          <tr>
-            <th style="width: 400px;"></th>
-            <th style="text-align: center; width: 100px;">Jan</th>
-            <th style="text-align: center; width: 100px;">Mar</th>
-            <th style="text-align: center; width: 100px;">Feb</th>
-            <th style="text-align: center; width: 100px;">Apr</th>
-            <th style="text-align: center; width: 100px;">May</th>
-            <th style="text-align: center; width: 100px;">Jun</th>
-            <th style="text-align: center; width: 100px;">Jul</th>
-            <th style="text-align: center; width: 100px;">Aug</th>
-            <th style="text-align: center; width: 100px;">Sep</th>
-            <th style="text-align: center; width: 100px;">Oct</th>
-            <th style="text-align: center; width: 100px;">Nov</th>
-            <th style="text-align: center; width: 100px;">Dec</th>
-          </tr>
+      <h4 class="text-center">Expenses</h4>
+      <table style="margin-bottom: 5%;" id="tableExpenses" class="table table-hover table-responsive">
+          
       </table>
       <br>
-      <table id="tableSavings" class="table table-hover table-responsive">
-          <tr>
-            <th style="width: 400px;"></th>
-            <th style="text-align: center; width: 100px;">Jan</th>
-            <th style="text-align: center; width: 100px;">Mar</th>
-            <th style="text-align: center; width: 100px;">Feb</th>
-            <th style="text-align: center; width: 100px;">Apr</th>
-            <th style="text-align: center; width: 100px;">May</th>
-            <th style="text-align: center; width: 100px;">Jun</th>
-            <th style="text-align: center; width: 100px;">Jul</th>
-            <th style="text-align: center; width: 100px;">Aug</th>
-            <th style="text-align: center; width: 100px;">Sep</th>
-            <th style="text-align: center; width: 100px;">Oct</th>
-            <th style="text-align: center; width: 100px;">Nov</th>
-            <th style="text-align: center; width: 100px;">Dec</th>
-          </tr>
+      <h4 class="text-center">Savings</h4>
+      <table style="margin-bottom: 5%;" id="tableSavings" class="table table-hover table-responsive">
+          
       </table>
       
     </div>
     <script>
-
-    function displayCategories() {
-      $('#tableIncome').empty();
-      $('#tableExpenses').empty();
-      $('#tableSavings').empty();
-      let header = ""+
-        "<tr>"+
-          "<th style=\"width: 400px;\"></th>"+
-          "<th style=\"text-align: center; width: 100px;\">Jan</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Mar</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Feb</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Apr</th>"+
-          "<th style=\"text-align: center; width: 100px;\">May</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Jun</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Jul</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Aug</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Sep</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Oct</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Nov</th>"+
-          "<th style=\"text-align: center; width: 100px;\">Dec</th>"+
-        "</tr>";
-        
-      $('#tableIncome').append(header);
-      $('#tableExpenses').append(header);
-      $('#tableSavings').append(header);
-      $.ajax({
-        url: 'model/getCategories.php',
-        success: function(data) {
-          let clearData = JSON.parse(data);
-          if (clearData.length == 0) {
-            
-          }
-          else {
-          clearData.forEach(function (item) {
-            if (item[2] == "Income") {
-              $('#tableIncome').append(""+
-                "<tr>"+
-                  "<td class=\"text-center\">Salaire (net)</td>"+
-                  "<td class=\"text-center\"><input type=\"text\" style=\"text-align: right;\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                "</tr>"
-              );
-            }
-            else if (item[2] == "Expenses") {
-              $('#tableExpenses').append(""+
-                "<tr>"+
-                  "<td class=\"text-center\">Salaire (net)</td>"+
-                  "<td class=\"text-center\"><input type=\"text\" style=\"text-align: right;\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                "</tr>"
-              );
+    
+      function getBudget(idCategory, year) {
+        $.ajax({
+          url: 'model/getBudgetFromCategoryAndYear.php',
+          data: {idCategory: idCategory, year: year},
+          success: function(data) {
+            let clearData = JSON.parse(data);
+            if (clearData.length == 0) {
+              alert("s");
             }
             else {
-              $('#tableSavings').append(""+
-                "<tr>"+
-                  "<td class=\"text-center\">Salaire (net)</td>"+
-                  "<td class=\"text-center\"><input type=\"text\" style=\"text-align: right;\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                  "<td class=\"text-center\"><input type=\"text\"></td>"+
-                "</tr>"
-              );
+              clearData.forEach(function (item) {
+                alert(item[1]);
+              });
             }
             
+            return clearData;
+          }
         });
       }
-        },
-        type: 'GET'
-      });
-    }
 
-    $(document).ready(function(){
-      
-      displayCategories();
-
-      //Add a Category
-      /*$("#addCat").click(function(){
+      function displayCategories() {
+        $('#tableIncome').empty();
+        $('#tableExpenses').empty();
+        $('#tableSavings').empty();
+        let header = ""+
+          "<tr>"+
+            "<th style=\"width: 400px;\"></th>"+
+            "<th style=\"text-align: center; width: 100px;\">Jan</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Mar</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Feb</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Apr</th>"+
+            "<th style=\"text-align: center; width: 100px;\">May</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Jun</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Jul</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Aug</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Sep</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Oct</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Nov</th>"+
+            "<th style=\"text-align: center; width: 100px;\">Dec</th>"+
+          "</tr>";
+          
+        $('#tableIncome').append(header);
+        $('#tableExpenses').append(header);
+        $('#tableSavings').append(header);
         $.ajax({
-         url: 'model/addCategory.php',
-         data: {nameCat: $("#nameCat").val(), typeCat: $("#typeCat").val()},
-         success: function(data) {
-           let clearData = JSON.parse(data);
-           if (clearData) {
-             getCategories();
-           }
-           else {
-             alert("ERROR: La categorie n'a pas été ajoutée");
-           }
-         },
-         type: 'GET'
+          url: 'model/getCategories.php',
+          success: function(data) {
+            let clearData = JSON.parse(data);
+            if (clearData.length == 0) {
+              
+            }
+            else {
+            clearData.forEach(function (item) {
+              let budget = getBudget(item[0], "2025");
+              if (item[2] == "Income") {
+                $('#tableIncome').append(""+
+                  "<tr>"+
+                    "<td class=\"text-center\">"+item[1]+"</td>"+
+                    "<td class=\"text-center\"><input type=\"text\" style=\"text-align: right;\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                  "</tr>"
+                );
+              }
+              else if (item[2] == "Expenses") {
+                $('#tableExpenses').append(""+
+                  "<tr>"+
+                    "<td class=\"text-center\">"+item[1]+"</td>"+
+                    "<td class=\"text-center\"><input type=\"text\" style=\"text-align: right;\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                  "</tr>"
+                );
+              }
+              else {
+                $('#tableSavings').append(""+
+                  "<tr>"+
+                    "<td class=\"text-center\">"+item[1]+"</td>"+
+                    "<td class=\"text-center\"><input type=\"text\" style=\"text-align: right;\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                    "<td class=\"text-center\"><input type=\"text\"></td>"+
+                  "</tr>"
+                );
+              }
+              
+          });
+        }
+          },
+          type: 'GET'
         });
-      });*/
-    });
-  </script>
+      }
+      
+
+      $(document).ready(function(){
+        
+        displayCategories();
+
+        //Add a Category
+        /*$("#addCat").click(function(){
+          $.ajax({
+          url: 'model/addCategory.php',
+          data: {nameCat: $("#nameCat").val(), typeCat: $("#typeCat").val()},
+          success: function(data) {
+            let clearData = JSON.parse(data);
+            if (clearData) {
+              getCategories();
+            }
+            else {
+              alert("ERROR: La categorie n'a pas été ajoutée");
+            }
+          },
+          type: 'GET'
+          });
+        });*/
+      });
+    </script>
   </body>
 </html>
