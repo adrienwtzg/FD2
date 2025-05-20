279c8a7a145cdac31adec70e56bf589c1cf1a43d
Adrien Witzig <adrien.wtzg@gmail.com>
2025-05-20 16:48:25 +0200

    pt2

--- views/savings.php
+++ views/savings.php
@@ -12,31 +12,88 @@ if (isset($_SESSION['loginError']))
     <style>
     
     </style>
-    <div class="marge" style="padding-right: 90%; padding-left: 1%; padding-top: 1%;">
-      <select class="custom-select" id="typeSavings">
-          <option selected>Prevision</option>
-          <option>Transactions</option>
-        </select>
-      </div>
+    <table  id="tableTransInput" style=" padding: 5px;" class="table table-borderless">
+          <tr>
+            <th class="col-2">
+              <select class="custom-select" id="type">
+                <option>Forecast</option>
+                <option>Transactions</option>
+              </select>
+            </th>
+            <th class="col-2">
+              <select class="custom-select" id="category">
+                <option>Salaire (net)</option>
+                <option>Courses</option>
+                <option>Assurance maladie</option>
+              </select>
+            </th>
+            <th class="col-8"></th>
+          </tr>
+        </table>
     <div class="row">
       <div class="col-2">
         
       </div>
       <div class="col-2">
-        <select class="custom-select" id="saving">
-          <option selected>Vacances</option>
-          <option>2026</option>
-        </select>
+        
       </div>
       
     </div>
     <br>
     <div class="row">
-      <div class="col-2"></div>
-      <div class="col-8" style="background-color: white; padding: 20px;">
+      <div class="col-1"></div>
+      <div class="col-6" style="background-color: white; padding: 20px; border-radius: 15px;">
+        <h4 style="text-align: center;">Forecast Vacances</h4>
+        <br>
         <canvas id="myChart" style="width:100%"></canvas>
       </div>
-      <div class="col-2"></div>
+      <div class="col-4" style="padding-left: 5%;">
+        <h5 style="text-align: center;">Projets</h5>
+        <br>
+        <table class="table">
+        
+        <tbody class="table-group-divider table-divider-color">
+          <tr>
+            <th scope="row">Vacances Tessin</th>
+            <td>Jul 25</td>
+            <td>1'000</td>
+            <td>
+              <div class="custom-control custom-checkbox">
+                  
+                  <button class="btn btn-outline-secondary btn-sm" style="border: 0;">📝</button>
+                  <button class="btn btn-outline-secondary btn-sm" style="border: 0;">❌</button>
+                </div>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Voyage Floride</th>
+            <td>Mai 26</td>
+            <td>10'000</td>
+            <td>
+              <div class="custom-control custom-checkbox">
+                  
+                  <button class="btn btn-outline-secondary btn-sm" style="border: 0;">📝</button>
+                  <button class="btn btn-outline-secondary btn-sm" style="border: 0;">❌</button>
+                </div>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Ski Zermatt 4 jours</th>
+            <td>Jan 26</td>
+            <td>1'000</td>
+            <td>
+              <div class="custom-control custom-checkbox">
+                  
+                  <button class="btn btn-outline-secondary btn-sm" style="border: 0;">📝</button>
+                  <button class="btn btn-outline-secondary btn-sm" style="border: 0;">❌</button>
+                </div>
+            </td>
+          </tr>
+          
+        </tbody>
+      </table>
+      </div>
+      <div class="col-1"></div>
       
     </div>
   </body>
@@ -60,7 +117,7 @@ if (isset($_SESSION['loginError']))
       options: {
         legend: {display: false},
         scales: {
-          yAxes: [{ticks: {min: 6, max:16}}],
+          yAxes: [{ticks: {min: 100, max: 1000}}],
         }
       }
     });
